1fb7ebbb7fa412fe47d30944edda894408619a58
Maximilian Keller <greenmax02@gmail.com>
2023-10-19 14:38:51 +0100

    herolanding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './sections/welcome/welcome.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
